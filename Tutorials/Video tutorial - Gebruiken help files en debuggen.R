d4d9973fa0bdeaf7f17effc74da8603b3020bab8
Doctah-E <e.j.van.ingen@vu.nl>
2024-01-04 10:04:14 +0100

    Setting up project

---
+++ Tutorials/Video tutorial - Gebruiken help files en debuggen.R
@@ -0,0 +1,9 @@
+# Video tutorial: gebruiken help files en debuggen
+
+
+
+
+# Gebruik tab toets
+# voor code completion maar ook voor arguments
+
+
---
+++ Tutorials/Video tutorial - Hercoderen variabelen.R
@@ -0,0 +1,20 @@
+# Video tutorial: veelgebruikte data transformaties
+
+
+# Rekenkundige transformaties
+
+
+
+# Recode (numeriek, character, factor)
+
+
+# Omzetten van variabele naar ander type
+
+
+# Aanpassingen in factor variabelen
+
+
+
+# Voorwaardelijke aanpassingen
+
+
